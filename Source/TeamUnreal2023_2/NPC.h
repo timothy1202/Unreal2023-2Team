8bc0fa4acc5c5171f8b47543156933f1c2be8347
timothy1202 <timothy1202@naver.com>
2023-11-27 15:10:33 +0900

    NPC 기본 생성
    영상 1번째

---
+++ Source/TeamUnreal2023_2/NPC.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "NPC.generated.h"
+
+UCLASS()
+class TEAMUNREAL2023_2_API ANPC : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ANPC();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+};
