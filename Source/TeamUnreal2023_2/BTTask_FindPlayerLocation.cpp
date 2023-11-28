8b8ba14f643602ba2178661709b33ece8ee493ec
timothy1202 <timothy1202@naver.com>
2023-11-28 20:01:17 +0900

    플레이어 위치 찾는 테스크 구현

---
+++ Source/TeamUnreal2023_2/BTTask_FindPlayerLocation.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_FindPlayerLocation.h"
+
+#include "NavigationSystem.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+
+
+UBTTask_FindPlayerLocation::UBTTask_FindPlayerLocation(FObjectInitializer const& ObjectInitializer)
+{
+	NodeName = TEXT("Find Player Location");
+}
+
+EBTNodeResult::Type UBTTask_FindPlayerLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	//get player character
+	if (auto* const Player = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)) // GameplayStatics으로 플레이어 캐릭터를 가져옴, 월드랑 플레이 명수도 가져옴
+	{
+		//get player location to use as an origin
+		auto const PlayerLocation = Player->GetActorLocation();
+		if (SearchRandom)
+		{
+			FNavLocation Loc;
+
+			//get the navigation system and generate a random location near the player
+			if (auto* const NavSys = UNavigationSystemV1::GetCurrent(GetWorld()))
+			{
+				//try to get a random location near the player
+				if (NavSys->GetRandomPointInNavigableRadius(PlayerLocation, SearchRadius, Loc))
+				{
+					OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), Loc.Location);
+					FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+					return EBTNodeResult::Succeeded;
+				}
+			}
+		}
+		else
+		{
+			OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), PlayerLocation);
+			FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+			return EBTNodeResult::Succeeded;
+		}
+	}
+
+	return EBTNodeResult::Failed;
+}
