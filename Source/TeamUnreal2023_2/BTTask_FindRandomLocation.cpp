c4b5631dacf8211f4d48056ce58abdd0f418ee57
timothy1202 <timothy1202@naver.com>
2023-11-28 23:54:18 +0900

    구현 끝

---
+++ Source/TeamUnreal2023_2/BTTask_FindRandomLocation.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_FindRandomLocation.h"
+#include "NPCAIController.h"
+
+#include "NavigationSystem.h"
+#include "NPCAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_FindRandomLocation::UBTTask_FindRandomLocation(FObjectInitializer const& ObjectInitializer)
+{
+	NodeName = "Find Random Location In NavMesh";
+}
+
+EBTNodeResult::Type UBTTask_FindRandomLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	//get AI Conroller and its npc
+	if (auto* const cont = Cast<ANPCAIController>(OwnerComp.GetAIOwner())) //폰의 컨트롤러를 가져와서 트리를 가져옴
+	{
+		if (auto* const npc = cont->GetPawn())
+		{
+			//obatin npc location to use as an origin
+			auto const Origin = npc->GetActorLocation();
+
+			//get the navigation system and generate a radom location
+			if (auto* const NavSys = UNavigationSystemV1::GetCurrent(GetWorld()))
+			{
+				FNavLocation Loc;
+				//랜덤위치를 찾으면 if문 실행
+				
+				if (NavSys->GetRandomPointInNavigableRadius(Origin, SearchRadius, Loc))
+				{
+					cont->GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), Loc.Location);
+				}
+
+
+				//finish with success
+				FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+				return EBTNodeResult::Succeeded;
+			}
+		}
+	}
+
+	return EBTNodeResult::Failed;
+}
