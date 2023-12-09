9a55e3cac76531bf171fe87714d4eee200cd08a5
timothy1202 <timothy1202@naver.com>
2023-12-10 05:09:21 +0900

    ㅇㅇ

--- Source/TeamUnreal2023_2/TeamUnreal2023_2GameMode.cpp
+++ Source/TeamUnreal2023_2/TeamUnreal2023_2GameMode.cpp
@@ -7,7 +7,7 @@
 ATeamUnreal2023_2GameMode::ATeamUnreal2023_2GameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
