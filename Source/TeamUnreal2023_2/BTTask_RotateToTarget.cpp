1183a9ca77ba43e9f88d2dbe0ca6ae7bfdea4654
chunbonge <eimuc00@gmail.com>
2023-12-13 03:14:47 +0900

    계속됨 ㄱㄷ

--- Source/TeamUnreal2023_2/BTTask_RotateToTarget.cpp
+++ Source/TeamUnreal2023_2/BTTask_RotateToTarget.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+// ¾ø¾Ö¾ß µÊ
 
 #include "BTTask_RotateToTarget.h"
 #include "BTFunction.h"
--- Source/TeamUnreal2023_2/BTTask_RotateToTarget.h
+++ Source/TeamUnreal2023_2/BTTask_RotateToTarget.h
@@ -1,4 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+// ¾ø¾Ö¾ß µÊ
 
 #pragma once
 
